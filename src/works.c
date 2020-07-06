c1312a8e261e567d360477b5d74ca1e02c4870f3
Serenade1234 <shuhei.7018@gmail.com>
2020-07-06 21:32:01 +0900

    done

--- src/works.c
+++ src/works.c
@@ -5,9 +5,29 @@ int k;
 int A[100000];
 
 
-int p(m){
-  return 0;
+int p(int x){
+  int sum = 0, hand = 1;
+  for(int i=0; i<n; i++){
+
+    if(x >= A[i]){
+
+      if(x >= sum + A[i]){
+        sum += A[i];
+
+      }else{
+        hand++;
+        sum = A[i];
+      }
+
+    }else{
+      return 0;
+    }
+  }
+  /*探索中の時間にあわせると人があまる
+    <=> 時間はもっと削れる*/
+  return k >= hand;
 }
+
 
 int main(){
   int i, lb, ub;
@@ -16,11 +36,11 @@ int main(){
     scanf("%d", &A[i]);
   }
 
-  _sum = 0;
+  int _sum = 0;
   for(int i=0; i<n; i++) _sum += A[i];
 
   lb = 0;
-  ub = _sum; //???????????
+  ub = _sum;
 
   while(ub - lb > 1){
     
@@ -33,6 +53,8 @@ int main(){
     }
 
   }
+
+  printf("%d\n", ub);
 
   return 0;
 }
