cbc64f691643714dc40a7936d0aefc82426046a7
Serenade1234 <shuhei.7018@gmail.com>
2020-07-06 21:08:43 +0900

    done

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,28 @@ int n;
 int k;
 int A[100000];
 
+int isMidEnough(int i_capacity){
+  int requiredBag = 0;
+
+  for(int i=0; i < n; i++){
+    printf("%d ", A[i]);
+    if(A[i]%A[i_capacity] == 0){
+      requiredBag += A[i]/A[i_capacity];
+    }else{
+      requiredBag += A[i]/A[i_capacity] + 1;
+    }
+  }
+  //printf("min : %d, required : %d, bagN : %d\n", A[i_capacity], requiredBag, k);
+  
+  /*探索中の要素を最小としたときに必要なバッグの数が指定の数より小さい
+     <=> バッグはもっと多くて良い
+     <=> バッグに入る小さい量はもっと少なくて良い。
+     <=> 要件を満たす最小のりんごの数は今より前の位置にある。
+  */
+  
+  return requiredBag <= k;
+}
+
 
 int main(){
   int i, lb, ub;
@@ -12,6 +34,22 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = -1;
+  ub = n-1;
+
+  while(ub - lb > 1){
+    
+    int mid = (ub + lb) / 2;
+
+    if(isMidEnough(mid)){
+      ub = mid;
+    }else{
+      lb = mid;
+    }
+
+  }
+
+  printf("%d\n", A[ub]);
 
   return 0;
 }
--- src/spear.c
+++ src/spear.c
@@ -5,6 +5,17 @@ int k;
 int A[100000];
 
 
+int isKEnoughLong(int thisLong){
+  
+  int spearSum = 0;
+  for(int i=0; i<n; i++) spearSum += A[i] / thisLong;
+
+  //printf("produce:%d, need:%d\n", spearSum, k);
+  /*作られる槍が必要量より少ない <=> 一本あたりはもっと小さくて良い*/
+  return spearSum < k;
+}
+
+
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
@@ -12,6 +23,22 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = A[n-1];
+
+  while(ub - lb > 1){
+    
+    int mid = (ub + lb) / 2;
+    //printf("%d < %d < %d\n", lb, mid, ub);
+    if(isKEnoughLong(mid)){
+      ub = mid;
+    }else{
+      lb = mid;
+    }
+
+  }
+
+  printf("%d\n", lb);
 
   return 0;
 }
